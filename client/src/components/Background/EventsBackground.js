3ff5c3595b02cfe3792b91726b46c19f87b56c95
bwtollestr <bwtollestrup@gmail.com>
2018-09-28 19:13:55 -0600

    eventspage

---
+++ client/src/components/Background/EventsBackground.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+
+const styles = theme => ({
+    
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    marginRight: 30,
+    marginLeft: 30,
+    height: 900,
+    marginTop: 10,
+    marginBottom: 10,
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }
+});
+
+function Background(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={12}>
+
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+
+Background.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Background);
---
+++ client/src/components/Buttons/Buttons.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+function ContainedButtons(props) {
+  const { classes } = props;
+  return (
+    <div>
+      <Button variant="contained" className={classes.button}>
+        Find Events
+      </Button>
+      {/* <Button variant="contained" color="primary" className={classes.button}>
+      Find Events
+      </Button> */}
+    </div>
+  );
+}
+
+ContainedButtons.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ContainedButtons);
