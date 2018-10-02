4ad9174feb9d74db16c727c230c2119946fc38ee
candice <candiceschramm@gmail.com>
2018-10-01 20:52:30 -0600

    merging my branch with styling on event-details page

--- client/src/components/EventDetails/EventDescription.js
+++ client/src/components/EventDetails/EventDescription.js
@@ -1,37 +1,43 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
-const EventInfo = props => (
-    <div>   
-        <div>
-            {props.title ? (
-                <div>
-                    {props.title}
-                </div>) : (
-                <div>
-                    No title abalible avalible at this time
-                </div>)    
-            }
-        </div>
-        <div>
-            {props.description ? ( 
-                <div>
-                    {props.description}
-                </div>) : (
-                <div>
-                    No description avalible at this time
-                </div>)
-            }
-        </div>
-        <div>
-            {/* <Map/> */} This is where a map will go
-        </div>
-        <button>
-            Not Interested
-        </button>
-        <button>
-            Definitly Going
-        </button>
-    </div>  
-);
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+});
 
-export default EventInfo;
+function EventDescription(props) {
+  const { classes } = props;
+  return (
+
+  <div>
+    <Paper className={classes.root} elevation={1}>
+      <Typography variant="headline" component="h3">
+        {props.title ? (
+          <div>{props.title}</div>
+        ) : (
+          <div>No title abalible avalible at this time</div>
+        )}
+      </Typography>
+      <Typography component="p">
+        {props.description ? (
+          <div>{props.description}</div>
+        ) : (
+          <div>No description avalible at this time</div>
+        )}
+      </Typography>
+    </Paper>
+  </div>
+  )};
+
+EventDescription.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(EventDescription);
--- client/src/components/EventDetails/EventDetails.js
+++ client/src/components/EventDetails/EventDetails.js
@@ -1,40 +1,44 @@
 import React, { Component } from "react";
-import EventInfo from "./EventDescription";
-import PotentialSquad from "./Attendees";
-import Chat from "./Comment";
+import EventDescription from "./EventDescription";
+import Attendees from "./Attendees";
+import Comment from "./Comment";
 import API from "../../utils/API";
+import ReusableNav from "../Nav/ReusableNav";
+import Grid from "@material-ui/core/Grid";
+
 // imports for nav and footer needed
 
 class EventDetails extends Component {
-    state = {
-        info: [],
-        id: this.props.match.params.id
-    };
-    componentWillMount() {
-        API.searchSingle(this.props.match.params.id)
-          .then(res => this.setState({ info: res.data}))
-          .catch(err => console.log(err));
-    }
+  state = {
+    info: [],
+    id: this.props.match.params.id
+  };
+  componentWillMount() {
+    API.searchSingle(this.props.match.params.id)
+      .then(res => this.setState({ info: res.data }))
+      .catch(err => console.log(err));
+  }
 
-    render(){
-        return(
-            <div>
-                {/* Nav will go here */}
-                <EventInfo
-                title={this.state.info.title}
-                description={this.state.info.description}
-                >
-                </EventInfo>
-                <PotentialSquad
-                id={this.state.id}>
-                </PotentialSquad>
-                <Chat
-                id={this.state.id}>
-                </Chat>
-                {/* Footer will go here */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ReusableNav navbarTitle="#GetOutMore" />
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Grid item xs={8}>
+            <EventDescription
+              title={this.state.info.title}
+              description={this.state.info.description}
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <Attendees id={this.state.id} />
+          </Grid>
+        </Grid>
+        <Comment id={this.state.id} />
+        {/* Footer will go here */}
+      </div>
+    );
+  }
 }
 
 export default EventDetails;
